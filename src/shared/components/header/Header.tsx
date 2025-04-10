21b161839b858f83b8eaca7737c14ad208b28573
marcoskisto <marcosoiseau@gmail.com>
2025-04-10 17:07:39 -0300

    feat Task 81: Inclui link para o manual de usuário

--- src/shared/components/header/Header.tsx
+++ src/shared/components/header/Header.tsx
@@ -12,7 +12,8 @@ import { DashboardOutlined,
     UserOutlined, 
     MenuOutlined, 
     SearchOutlined, 
-    FileExcelOutlined} from '@ant-design/icons';
+    FileExcelOutlined,
+    QuestionCircleOutlined} from '@ant-design/icons';
 
 import logo from '../../../../public/kersys.png';
 import icon from '../../../../public/kersys.png';
@@ -84,7 +85,13 @@ const Header = () => {
                 ], 
               }
             ]
-        }
+        },
+        {
+            key: 'external_link',
+            icon: <QuestionCircleOutlined />,
+            label: 'Ajuda',
+            onClick: () => window.open('https://localhost-308.github.io/manual/', '_blank')
+          }
     ];
 
     return (
